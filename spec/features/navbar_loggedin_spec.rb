51140ba1981f7b7e32c63160141136e248975b58
Alan <alan.vey12@imperial.ac.uk>
2014-05-30 20:39:03 +0100

    Implemented Overview and Edit links

--- spec/features/navbar_loggedin_spec.rb
+++ spec/features/navbar_loggedin_spec.rb
@@ -11,18 +11,40 @@ feature 'View a page with the navbar when signed in' do
     current_path.should eq('/')
   end
 
-  scenario 'click name to visit profile' do
+  scenario 'and click name to visit profile' do
     click_link @user.email
     fill_in 'Password', with: 'abcdefghijklmnop'
     fill_in 'Password confirmation', with: 'abcdefghijklmnop'
     fill_in 'Current password', with: @user.password
     click_button 'Update'
+
     current_path.should eq(projects_path)
   end
 
-  scenario 'navigate to root page' do
+  scenario 'and navigate to root page' do
     click_link @user.email
     click_link "Pjmanagement"
+
     current_path.should eq('/projects')
   end
+
+  scenario 'and check overview and edit links appear' do
+    create_project
+
+    expect(page).to have_content('Overview')
+    expect(page).to have_content('Edit')
+  end
+
+  scenario 'and click overview' do
+    create_project
+    @prev_path = current_path
+
+    click_link 'Overview'
+  
+    current_path.should eq(@prev_path)
+  end
+
+  # Add code when OT finishes his part
+  scenario 'and click edit' 
+
 end
