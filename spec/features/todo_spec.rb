186fe168e43fbdfef00fbb2ba40edac836afed0e
Alan <alan.vey12@imperial.ac.uk>
2014-06-05 15:20:15 +0100

    Provides the latex file for the final report

--- spec/features/todo_spec.rb
+++ spec/features/todo_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 feature 'Still need to be done: ' do
   scenario 'Automatically set status of a milestone based on sub tasks'
+  scenario 'Generate priorities array on sub tasks and order accordingly'
+  scenario 'Create buttons for start, finish, review (Owner) subtasks'
 end
