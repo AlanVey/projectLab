2ffe4acb1d61ff85cf93ebc23b5094a39640bcef
Alan <alan.vey12@imperial.ac.uk>
2014-06-06 16:48:27 +0100

    Comments out issues causing site to crash when navigating to /projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,12 +8,12 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @search = Project.where(user: current_user).search do
-      fulltext params[:search]
-    end
-    @projects = @search.results
+    #@search = Project.where(user: current_user).search do
+      #fulltext params[:search]
+    #end
+    #@projects = @search.results
 
-    #@projects = Project.where(user: current_user)
+    @projects = Project.where(user: current_user)
     ProjectUser.where(user_id: current_user).each do |pjuser|
       Project.where(id: pjuser.project_id).each { |pj| @projects << pj }
     end 
